43a1120013fc7bcee18177f42be9fb5cb8737a4a
Evangelos Zotos <evangelos.zotos@googlemail.com>
2022-03-17 11:05:13 +0100

    - Task 30

---
+++ webapp/pages/account/signin.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "@mui/material";
+import SignIn from "../../containers/SignIn";
+
+const SignInPage: React.FC = (props) => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  return (
+    <Container sx={{ mt: 24 }} maxWidth="md">
+      <SignIn
+        onSignIn={({ email, password }) => console.log(email, password)}
+      />
+    </Container>
+  );
+};
+
+export default SignInPage;
