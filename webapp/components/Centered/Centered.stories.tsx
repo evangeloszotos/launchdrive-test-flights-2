5a818724ae0c643eec319fdf42398ad232b71bb0
Lars Weimer <lars@launchlytics.io>
2022-03-17 17:44:07 +0100

    Changed styling from sx to styled

--- webapp/components/Centered/Centered.stories.tsx
+++ webapp/components/Centered/Centered.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Box, Paper } from '@mui/material';
-import { Centered } from './Centered';
+import { Centered, CenteredHorizontallyOnly } from './Centered';
 
 export default {
   title: 'Components/Centered',
@@ -10,11 +10,20 @@ export default {
 
 const Template: ComponentStory<typeof Centered> = () => (
   <Box sx={{ border: '1px solid red', width: 200, height: 200 }}>
+    <CenteredHorizontallyOnly>
+      <Paper sx={{ width: 32, height: 32 }} />
+    </CenteredHorizontallyOnly>
+  </Box>
+);
+export const Unstyled = Template.bind({});
+Unstyled.args = {};
+
+const Template2: ComponentStory<typeof Centered> = () => (
+  <Box sx={{ border: '1px solid red', width: 200, height: 200 }}>
     <Centered>
       <Paper sx={{ width: 32, height: 32 }} />
     </Centered>
   </Box>
 );
-
-export const Default = Template.bind({});
+export const Default = Template2.bind({});
 Default.args = {};
--- webapp/components/Centered/Centered.tsx
+++ webapp/components/Centered/Centered.tsx
@@ -1,25 +1,21 @@
 import PropTypes from 'prop-types';
 import { Grid } from '@mui/material';
+import { styled } from '@mui/system';
 
-export const Centered = (props) => {
-  const { children, sx } = props;
-
-  return (
-    <Grid container={true} justifyContent="center" alignItems="center" {...sx}>
-      {children}
-    </Grid>
-  );
-};
-Centered.propTypes = {
+export const CenteredHorizontallyOnly = (props) => (
+  <Grid {...props} container={true} justifyContent="center" alignItems="center" />
+);
+CenteredHorizontallyOnly.propTypes = {
   children: PropTypes.node,
   sx: PropTypes.object,
 };
-Centered.defaultProps = {
-  children: [],
-  sx: {
-    width: '100%',
-    height: '100%',
-  },
+CenteredHorizontallyOnly.defaultProps = {
+  children: null,
+  sx: {},
 };
 
+export const Centered = styled(CenteredHorizontallyOnly)`
+  width: 100%;
+  height: 100%;
+`;
 export default Centered;
