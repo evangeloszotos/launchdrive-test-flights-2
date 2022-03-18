d63f483c4a0350c2d017bd1acf3cada0eb8bb239
Sven Feierabend <sven@launchlytics.io>
2022-03-18 10:37:33 +0100

    Merge remote-tracking branch 'origin/main'

--- webapp/.storybook/main.js
+++ webapp/.storybook/main.js
@@ -4,7 +4,12 @@ module.exports = {
     '../stories/**/*.stories.(js|jsx|ts|tsx)',
     '../components/**/*.stories.(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-next-router',
+  ],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
---
+++ webapp/components/Centered/Centered.tsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { Grid } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const CenteredHorizontallyOnly = (props) => (
+  <Grid {...props} container={true} justifyContent="center" alignItems="center" />
+);
+CenteredHorizontallyOnly.propTypes = {
+  children: PropTypes.node,
+  sx: PropTypes.object,
+};
+CenteredHorizontallyOnly.defaultProps = {
+  children: null,
+  sx: {},
+};
+
+export const Centered = styled(CenteredHorizontallyOnly)`
+  width: 100%;
+  height: 100%;
+`;
+export default Centered;
