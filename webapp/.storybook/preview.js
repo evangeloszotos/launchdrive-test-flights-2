f6cde9c3cac6075c3d3c3ff816e4424c8ef93cf2
Lars Weimer <lars@launchlytics.io>
2022-03-18 10:34:45 +0100

    Merged main

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -1,26 +1,26 @@
-import "../styles/globals.css";
-import * as NextImage from "next/image";
-import { RouterContext } from "next/dist/shared/lib/router-context";
-import {ThemeProvider} from "@mui/material/styles";
-import {theme} from '../mui-theme'
+import '../styles/globals.css';
+import * as NextImage from 'next/image';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../mui-theme';
 
 const OriginalNextImage = NextImage.default;
 
-Object.defineProperty(NextImage, "default", {
+Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
 
 export const decorators = [
   (Story) => (
-      <ThemeProvider theme={theme}>
-        <Story />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
   ),
-]
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
