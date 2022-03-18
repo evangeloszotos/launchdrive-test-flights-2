5fad3fd9bd918567d8ccc1f3bb6b70f548392613
Lars Weimer <lars@launchlytics.io>
2022-03-18 11:47:17 +0100

    Added events to storyboox explicitly

--- webapp/components/ColorTextField/ColorTextField.stories.tsx
+++ webapp/components/ColorTextField/ColorTextField.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { ColorTextField, ColorTextFieldUnstyled } from './ColorTextField';
 
 export default {
@@ -12,6 +13,8 @@ export const Default = Template.bind({});
 Default.args = {
   label: 'Primary',
   value: '#0fa',
+  onInput: action('onInput'),
+  onValidInput: action('onValidInput'),
 };
 
 const TemplateUnstyled: ComponentStory<typeof ColorTextFieldUnstyled> = (args) => <ColorTextFieldUnstyled {...args} />;
@@ -19,4 +22,6 @@ export const Unstyled = TemplateUnstyled.bind({});
 Unstyled.args = {
   label: 'Primary',
   value: '#0fa',
+  onInput: action('onInput'),
+  onValidInput: action('onValidInput'),
 };
