83aacc21aea3a1c715c44b7116865a3db1d9bf33
Lars Weimer <lars@launchlytics.io>
2022-03-18 11:24:34 +0100

    Removed overengineered options from DesktopMobileToggle

--- webapp/components/DeskopMobileToggle/DesktopMobileToggle.tsx
+++ webapp/components/DeskopMobileToggle/DesktopMobileToggle.tsx
@@ -1,26 +1,16 @@
 import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { DesktopWindows, Smartphone } from '@mui/icons-material';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 
 export const DesktopMobileToggle = (props) => {
-  const { onChange, state, triggerOnChangeOnNullValue, handleStateInternally } = props;
-  const [internalState, setInternalState] = useState(state);
+  const { onChange, state } = props;
 
   const handleChange = (e, v) => {
-    if (triggerOnChangeOnNullValue || v !== null) {
-      if (handleStateInternally && v !== null) setInternalState(v);
-      onChange(v);
-    }
+    onChange(v);
   };
 
   return (
-    <ToggleButtonGroup
-      size="small"
-      value={handleStateInternally ? internalState : state}
-      exclusive={true}
-      onChange={handleChange}
-    >
+    <ToggleButtonGroup size="small" value={state} exclusive={true} onChange={handleChange}>
       <ToggleButton value="desktop">
         <DesktopWindows sx={{ mr: 0.75 }} />{' '}
         <Typography sx={{ fontSize: '13px', fontWeight: 'medium', letterSpacing: '0.46px' }}> Desktop </Typography>
@@ -35,14 +25,10 @@ export const DesktopMobileToggle = (props) => {
 DesktopMobileToggle.propTypes = {
   state: PropTypes.oneOf(['desktop', 'mobile']),
   onChange: PropTypes.func,
-  triggerOnChangeOnNullValue: PropTypes.bool,
-  handleStateInternally: PropTypes.bool,
 };
 DesktopMobileToggle.defaultProps = {
   state: 'desktop',
   onChange: () => {},
-  triggerOnChangeOnNullValue: false,
-  handleStateInternally: false,
 };
 
 export default DesktopMobileToggle;
