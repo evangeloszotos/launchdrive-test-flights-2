0f056c5c44767ce2a1442e65685ef9229b148e57
Sven Feierabend <sven@launchlytics.io>
2022-03-22 14:12:56 +0100

    adjusted defaultProps;

--- webapp/components/TemplatePreviewDialog/TemplatePreviewDialogAppBar.tsx
+++ webapp/components/TemplatePreviewDialog/TemplatePreviewDialogAppBar.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { AppBar, AppBarProps, Grid, styled, Toolbar } from '@mui/material';
 
-interface TemplatePreviewDialogAppBarProps extends AppBarProps {
-  startContent: React.ReactNode;
-  centerContent: React.ReactNode;
-  endContent: React.ReactNode;
-}
-
 const AppBarStyled = styled(AppBar)`
   color: #000000de;
   background-color: white;
   box-shadow: 0px 1px 0px #dddfe5;
 `;
 
-export const TemplatePreviewDialogAppBar: React.FC<TemplatePreviewDialogAppBarProps> = (props) => {
+interface TemplatePreviewDialogAppBarProps extends AppBarProps {
+  startContent: React.ReactNode;
+  centerContent: React.ReactNode;
+  endContent: React.ReactNode;
+}
+
+const defaultProps = {
+  position: 'sticky',
+  elevation: 0,
+};
+
+export const TemplatePreviewDialogAppBar: React.FC<TemplatePreviewDialogAppBarProps & typeof defaultProps> = (
+  props
+) => {
   const { startContent, centerContent, endContent, ...other } = props;
 
   return (
-    <AppBarStyled position="sticky" elevation={0} {...other}>
+    <AppBarStyled {...other}>
       <Toolbar>
         <Grid container={true} direction="row" alignItems="center">
           <Grid item={true} sm={4}>
